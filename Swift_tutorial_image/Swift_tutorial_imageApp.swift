93f1c21a59af86a1d7112ad51b3c9d692b97fd26
Jun Hyeong An <99971427+JunHyeongAn@users.noreply.github.com>
2022-05-27 15:25:14 +0900

    Initial Commit

---
+++ Swift_tutorial_image/Swift_tutorial_imageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_tutorial_imageApp.swift
+//  Swift_tutorial_image
+//
+//  Created by 안준형 on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct Swift_tutorial_imageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
